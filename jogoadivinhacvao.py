105383a508b9bf8b576b550a12e34b3e3e06a0ad
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-02-28 09:50:03 -0300

    Add files via upload

--- jogoadivinhacvao.py
+++ jogoadivinhacvao.py
@@ -2,14 +2,25 @@ print('********************************')
 print('bem vindo ao JOGO DE ADIVINHAÇÃO')
 print('********************************')
 
+#definindo o numero secreto
 numerosecreto= 16
 
-chute= input('Digite um número')
+#definindo o numero de tentativas
+numerotentativas= 3
 
-print('Você digitou o número', chute)
+while(numero de tentativas > 0):
+    print('ok')
 
+#recebendo o chute do jogador
+    chuteString= input('Digite um número')
+
+    print('Você digitou o número', chute)
+
+#declarando as condições
 if numerosecreto == chute:
     print('Você acertou!')
 
-else:
-    print('Você errou!')
+elif(chute>numerosecreto):
+    print('você errou! O numero secreto é um numero menor')
+else: 
+    print('você errou! o numero secreto é um numero maior')    

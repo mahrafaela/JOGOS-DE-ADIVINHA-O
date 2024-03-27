5e568dd5f3beae9f7bbbf58db0d77af68b277993
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-03-27 08:57:12 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -1,21 +1,25 @@
+import random;
+
 print('********************************')
 print('bem vindo ao JOGO DE ADIVINHAÇÃO')
 print('********************************')
 
 #definindo o numero secreto
-numerosecreto= 16
+numerosecreto= round(random.random()*100)
+print(numerosecreto)
 rodada = 1 
 #definindo o numero de tentativas
-numerotentativas= 3
+#numerotentativas= 10
 
 while(rodada <= 3):
-    print('tentativas ', rodada, 'de', numerotentativas)
+    print('tentativas', rodada, 'de', numerotentativas)
 
 #recebendo o chute do jogador
-    chuteString= input('Digite um número')
+    chuteString= input('Digite um número ente 1 a 100:')
+    chute = int(chuteString)
 
     #print('Você digitou o número', chuteString)
-    chute = int(chuteString)
+    
 
 #declarando as condições
     if (numerosecreto == chute):
@@ -24,7 +28,6 @@ while(rodada <= 3):
     elif(chute>numerosecreto):
         print('você errou! O numero secreto é um numero menor')
     else: 
-        print('você errou! o numero secreto é um numero maior') 
-    rodada = rodada+1       
+        print('você errou! o numero secreto é um numero maior')       
     #numerotentativas = numerotentativas - 1    
-
+    rodada = rodada+1   

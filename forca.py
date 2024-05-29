a1ab297eed5d0126b9233d59f0531bd8f84f19c5
mahrafaela <128821670+mahrafaela@users.noreply.github.com>
2024-05-29 08:55:36 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,5 +1,21 @@
-print('**********************************')
-print('Bem vindo, ao JOGO FORCA')
-print('**********************************')
+def jogar_forca():
 
-print("Fim de jogo")
+    print('*********************************')
+    print('Bem vindo, ao JOGO FORCA')
+    print('*********************************')
+    #Definir qual a palavra secreta
+    palavraSecreta = "pepino"
+
+    enforcou = False
+    acertou = False
+
+    #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
+    while(not enforcou and not acertou):
+
+        chute = input("Escolha uma letra!")
+        print("Escolha outra letra!")
+
+
+    print("Fim de jogo!")
+if(__name__ == "__main__"):
+    jogar_forca()
